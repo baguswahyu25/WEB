d49d90c16ee6a04009e4d91173a9b337dd20a1e9
baguswahyu25 <wahyugantng25@gmail.com>
2025-12-10 17:12:56 +0700

    update pendaftaran

--- routes/api.php
+++ routes/api.php
@@ -8,15 +8,13 @@ use App\Http\Controllers\Api\UserController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use App\Http\Controllers\Api\ForgotPasswordController;
 use Illuminate\Support\Facades\Password;
-use App\Http\Controllers\FormController;
-use App\Http\Controllers\FormSimController;
+use App\Http\Controllers\FormPendaftaranController;
 
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/user/update', [UserController::class, 'update']);
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/form-store', [FormController::class, 'store']);
     Route::post('/email/resend', [VerificationController::class, 'resend'])
         ->name('api.verification.resend');
 });
@@ -44,12 +42,16 @@ Route::post('/forgot-password', function (Request $request) {
 Route::post('/password/reset', [ForgotPasswordController::class, 'resetPassword'])
     ->name('api.password.reset');
 
-Route::post('form-sim', [FormSimController::class, 'store']);
 Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
     return response()->json([
         'success' => true,
         'user' => $request->user()
     ]);
+});
+Route::prefix('v1')->group(function () {
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/pendaftaran', [FormPendaftaranController::class, 'store']);
+    });
 });
 
 
